3667fdd99bbdd8da94f5594db110ef5053f14bb0
samuel sepulveda <samiaron.sepulveda@gmail>
2023-05-06 13:37:16 -0400

    loginxd

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import Login from "./pages/Login";
 
 import { FC } from "react";
 interface Route {
@@ -32,6 +33,13 @@ export const routes: Array<Route> = [
         path: '/about',
         enabled: true,
         component: About
+    },
+    {
+        key: 'login-route',
+        title: 'Login',
+        path: '/login',
+        enabled: true,
+        component: Login
     },
 ]
 
